99e3760e3c6579b716807efc22df8ffa1234821e
TL-Garcia <tomas.bl.garcia@gmail.com>
2023-05-04 17:38:20 +0200

    setup: Add missing files

---
+++ packages/chord-generator/index.ts
@@ -0,0 +1,73 @@
+export class CircleOfFifths {
+  constructor() {}
+
+  getChords() {
+    return [
+      {
+        notes: ["C", "E", "G", "B"],
+        name: "C major seventh",
+        symbol: "Cmaj7",
+      },
+      {
+        notes: ["F", "A", "C", "E"],
+        name: "F major seventh",
+        symbol: "Fmaj7",
+      },
+      {
+        notes: ["Bb", "D", "F", "A"],
+        name: "Bb major seventh",
+        symbol: "Bbmaj7",
+      },
+      {
+        notes: ["Eb", "G", "Bb", "D"],
+        name: "Eb major seventh",
+        symbol: "Ebmaj7",
+      },
+      {
+        notes: ["Ab", "C", "Eb", "G"],
+        name: "Ab major seventh",
+        symbol: "Abmaj7",
+      },
+      {
+        notes: ["Db", "F", "Ab", "C"],
+        name: "Db major seventh",
+        symbol: "Dbmaj7",
+      },
+      {
+        notes: ["Gb", "Bb", "Db", "F"],
+        name: "Gb major seventh",
+        symbol: "Gbmaj7",
+      },
+      {
+        notes: ["G", "B", "D", "F#"],
+        name: "G major seventh",
+        symbol: "Gmaj7",
+      },
+      {
+        notes: ["D", "F#", "A", "C#"],
+        name: "D major seventh",
+        symbol: "Dmaj7",
+      },
+      {
+        notes: ["A", "C#", "E", "G#"],
+        name: "A major seventh",
+        symbol: "Amaj7",
+      },
+      {
+        notes: ["E", "G#", "B", "D#"],
+        name: "E major seventh",
+        symbol: "Emaj7",
+      },
+      {
+        notes: ["B", "D#", "F#", "A#"],
+        name: "B major seventh",
+        symbol: "Bmaj7",
+      },
+      {
+        notes: ["F#", "A#", "C#", "E#"],
+        name: "F# major seventh",
+        symbol: "F#maj7",
+      },
+    ];
+  }
+}
