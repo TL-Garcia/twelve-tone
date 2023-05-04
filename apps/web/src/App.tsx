57f460a4c80fd772d1c7bce1fdaca3d388fbac4a
TL-Garcia <tomas.bl.garcia@gmail.com>
2023-05-04 17:30:37 +0200

    setup: Create workspace

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,31 @@
+import { CircleOfFifths } from "chord-generator";
+
+const NaturalFifths = new CircleOfFifths();
+
 function App() {
   return (
     <>
       <h1>Twelve tone music</h1>
+
+      <h2>Some chords</h2>
+
+      <dl>
+        {NaturalFifths.getChords().map((chord) => {
+          return (
+            <div>
+              <dt>
+                <b>Name: </b>
+                {chord.name}
+              </dt>
+              <dd>
+                {chord.notes.map((note, i) =>
+                  i !== chord.notes.length - 1 ? `${note} - ` : note
+                )}
+              </dd>
+            </div>
+          );
+        })}
+      </dl>
     </>
   );
 }
