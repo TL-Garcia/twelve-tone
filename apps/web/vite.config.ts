817d9ef10c81a61f360ba3338b3378796a721559
TL-Garcia <tomas.bl.garcia@gmail.com>
2023-05-06 12:58:23 +0200

    patch: Remove vite-stylelint-config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import nested from "postcss-nested";
-import StylelintPlugin from "vite-plugin-stylelint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), StylelintPlugin()],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [nested],
