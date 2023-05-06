4984199939d516a41ed7c896ac319ff673cd1538
TL-Garcia <tomas.bl.garcia@gmail.com>
2023-05-06 12:55:01 +0200

    feat: Add postcss & stylelint support

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import nested from "postcss-nested";
+import StylelintPlugin from "vite-plugin-stylelint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), StylelintPlugin()],
+  css: {
+    postcss: {
+      plugins: [nested],
+    },
+  },
 });
