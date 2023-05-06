806c4cbf94118ff525f30da4258eed9b0605cc94
TL-Garcia <tomas.bl.garcia@gmail.com>
2023-05-06 13:33:22 +0200

    feat: Add path aliases

--- apps/web/src/components/templates/chord-visualizer/ChordVisualizer.tsx
+++ apps/web/src/components/templates/chord-visualizer/ChordVisualizer.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Button } from "../atoms";
+import { Button } from "@/components";
 import { ChordData } from "./components";
 
 import { Chord } from "chord-generator";
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import nested from "postcss-nested";
+import { resolve } from "path";
+
+const projectRootDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(projectRootDir, "/src"),
+    },
+  },
   plugins: [react()],
   css: {
     postcss: {
